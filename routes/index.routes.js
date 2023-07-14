bdab3915ab25eda8801c125ec9f2b475a60c13ff
Jswears <ji.swearssalinas@gmail.com>
2023-07-14 09:59:41 +0200

    fixing cookies

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
+//Require Models
+const Habit = require("../models/Habits.model");
+
 //require middleware
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
 
@@ -8,10 +11,25 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/userProfile", isLoggedIn, (req, res) => {
-  res.render("users/user-profile", { userInSession: req.session.currentUser });
+router.get("/userProfile", isLoggedIn, async (req, res, next) => {
+  const habitList = await Habit.find({});
+
+  res.render("users/user-profile", {
+    userInSession: req.session.currentUser,
+    habitList,
+  });
 });
 
+router.post("/userProfile", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const { name } = req.body;
+    const newHabit = await Habit.create({ name });
+    res.redirect("/userProfile");
+  } catch (error) {
+    console.log("There has been an error: ", error);
+  }
+});
 
 router.post("/logout", (req, res) => {
   req.session.destroy((err) => {
